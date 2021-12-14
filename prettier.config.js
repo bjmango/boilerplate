56c93e2aacaed973871fc3e86524c306c5f217c4
Eric Yun <yunxiuyu@hotmail.com>
2021-12-14 13:38:26 -0700

    Create prettier.config.js

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+// https://prettier.io/docs/en/options.html
+module.exports = {
+  bracketSpacing: false,
+  jsxBracketSameLine: false,
+  printWidth: 120,
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'es5',
+};
